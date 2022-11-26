2dcfbc26732b9d3bc1459c3bfd2e3b2e0a49ebf9
Libing Chen <libing.chen@gmail.com>
2022-11-26 12:31:21 +0800

    app: User class

---
+++ src/main/java/org/mvnsearch/mybatisplus/domain/model/User.java
@@ -0,0 +1,11 @@
+package org.mvnsearch.mybatisplus.domain.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
