f1701c865bab9aa6e12aa106e7c8e1c3233cabf3
Libing Chen <libing.chen@gmail.com>
2022-11-26 12:33:15 +0800

    app(mybatis): UserMapper

---
+++ src/main/java/org/mvnsearch/mybatisplus/impl/infra/mybatis/UserMapper.java
@@ -0,0 +1,8 @@
+package org.mvnsearch.mybatisplus.impl.infra.mybatis;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.mvnsearch.mybatisplus.domain.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
