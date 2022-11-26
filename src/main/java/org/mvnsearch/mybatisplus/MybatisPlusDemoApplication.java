7585e8a3b71a6d5598210c3d9086821efbd99dbc
Libing Chen <libing.chen@gmail.com>
2022-11-26 12:31:37 +0800

    app: MybatisPlusDemoApplication

---
+++ src/main/java/org/mvnsearch/mybatisplus/MybatisPlusDemoApplication.java
@@ -0,0 +1,15 @@
+package org.mvnsearch.mybatisplus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.mvnsearch.mybatisplus.impl.infra.mybatis")
+public class MybatisPlusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDemoApplication.class, args);
+    }
+
+}
