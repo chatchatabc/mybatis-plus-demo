2ca30cf2d9dc400fd43a7c35d1ee14cf60da709a
Libing Chen <libing.chen@gmail.com>
2022-11-26 16:47:31 +0800

    chore: add annotations

--- src/main/java/org/mvnsearch/mybatisplus/domain/model/User.java
+++ src/main/java/org/mvnsearch/mybatisplus/domain/model/User.java
@@ -1,10 +1,17 @@
 package org.mvnsearch.mybatisplus.domain.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("user")
 public class User {
+    @TableId(type = IdType.AUTO)
     private Long id;
+    @TableField("name")
     private String name;
     private Integer age;
     private String email;
