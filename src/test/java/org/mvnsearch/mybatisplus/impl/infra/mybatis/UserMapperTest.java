709b130be607f0c346fdbc2d9c3b69c4a7e177bf
Libing Chen <libing.chen@gmail.com>
2022-11-26 16:47:21 +0800

    test: add testOperations

--- src/test/java/org/mvnsearch/mybatisplus/impl/infra/mybatis/UserMapperTest.java
+++ src/test/java/org/mvnsearch/mybatisplus/impl/infra/mybatis/UserMapperTest.java
@@ -5,6 +5,8 @@ import org.mvnsearch.mybatisplus.SpringBootBaseTest;
 import org.mvnsearch.mybatisplus.domain.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -18,5 +20,20 @@ public class UserMapperTest extends SpringBootBaseTest {
     public void testFindById() {
         final User user = userMapper.selectById(1);
         System.out.println(user.getName());
+    }
+
+    @Test
+    public void testOperations() {
+        User user = new User();
+        user.setAge(18);
+        user.setName("linux_china");
+        user.setEmail("kyoko_shrefflerbik@john.apw");
+        //insert
+        userMapper.insert(user);
+        System.out.println(user.getId());
+        //delete
+        userMapper.deleteById(user.getId());
+        // query
+        assertThat(userMapper.selectById(user.getId())).isNull();
     }
 }
